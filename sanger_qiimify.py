4a1985cca0c5c595944a314cde77e12d36b62e7c
Shareef Dabdoub <dabdoub.2@osu.edu>
2012-10-16 14:06:25 -0400

    The program is now functionally complete.
    Adds custom argparse Action subclass for validating the identifier
    pattern program option
    
    Adds a method open_enc() that opens a file using either the built-in
    open() or the open method found in the codecs module for opening UTF-16
    encoded files. This is now incorporated throughout the program.
    
    Adds gather_sample_ids() as an alternative to the
    gather_hierarchical_sample_ids(), to be used when all the sequence data
    is contained within a single folder
    
    The write_mapping_file() method uses a file handle instead of opening a
    file handle from an input file name. The program now uses the argparse
    module's ability to directly open a file during option parsing.
    
    The scrobble_fasta_dir() method now works for both hierarchical and
    single-directory layouts, and handles UTF-16 input
    
    The argparse parser now handles a --version flag to print out the
    program version, and is more user-friendly with the identifier pattern
    option. Finally it has options for instructing the program to look for
    quality files in addition to fasta files, and has a flag for handling
    UTF-16 input files
    
    Main now handles both hierarchy and single directory layouts.

--- sanger_qiimify.py
+++ sanger_qiimify.py
@@ -11,6 +11,7 @@ for the sample IDs and the generated barcodes (and primers).
 '''
 import argparse
 from collections import namedtuple
+import codecs
 import os
 import os.path as osp
 import sys
@@ -24,8 +25,36 @@ file_types = {'fasta': ('.fasta', '.fa', '.txt', '.seq', '.Seq'),
               'qual': ('.qual')}
 
 MapRecord = namedtuple('MapRecord', 'barcode primer descr')
+IDPattern = namedtuple('IDPattern','separator field')
 
-def gather_hierarchical_sample_ids(sampleDir):
+class ValidateIDPattern(argparse.Action):
+    def __call__(self, parser, args, values, option_string = None):
+        # print '{n} {v} {o}'.format(n=args, v=values, o=option_string)
+        separator, field = values
+        try:
+            field = int(field)
+        except ValueError:
+            print parser.format_usage().strip()
+            print '%s: error: FIELD_NUMBER must be a number' % sys.argv[0]
+            sys.exit(-1)
+        
+        setattr(args, self.dest, IDPattern(separator, field))
+
+
+def open_enc(fn, utf16):
+    """
+    Open a file for reading with either the standard built-in open() or the
+    open in the codecs module for reading UTF-16 encoded files.
+    
+    :@param fn: Name of the file to open
+    :@param utf16: Boolean flag to specify a UTF-16 file
+    """
+    if utf16:
+        return codecs.open(fn, encoding='utf-16')
+    return open(fn, 'rU')
+
+
+def gather_hierarchical_sample_ids(sampleDir, utf16):
     """
     Given a directory containing directories of Sanger sequenced 
     DNA, generate sample IDs from the directory names.
@@ -44,16 +73,38 @@ def gather_hierarchical_sample_ids(sampleDir):
                 if (osp.isfile(os.path.join(iPath, sitem)) and 
                     osp.splitext(sitem)[1] in file_types['fasta']):
                     try:
-                        SeqIO.read(os.path.join(iPath, sitem), 'fasta')
-                        dirInclude = True
-                        dirs.append(iPath)
-                        break
-                    except ValueError, _:
+                        with open_enc(os.path.join(iPath, sitem), utf16) as fh:
+                            SeqIO.read(fh, 'fasta')
+                            dirInclude = True
+                            dirs.append(iPath)
+                            break
+                    except ValueError:
                         pass
             if dirInclude:
                 sampleIDs.append(item)
     
     return sampleIDs, dirs
+
+
+def gather_sample_ids(sampleDir, idPattern, utf16):
+    """
+    
+    """
+    sampleIDs = []
+    separator, field = idPattern
+    for item in os.listdir(sampleDir):
+        iPath = os.path.join(sampleDir, item)
+        if (osp.isfile(iPath) and 
+            osp.splitext(item)[1] in file_types['fasta']):
+            try:
+                with open_enc(iPath, utf16) as fh:
+                    record = SeqIO.parse(fh, 'fasta').next()
+                    sampleIDs.append(record.id.split(separator)[field-1])
+            except StopIteration:
+                print 'Empty FASTA file: %s' % iPath
+    
+    return sampleIDs
+
 
 def generate_barcodes(nIds, codes=None, idx=-1, codeLen=12):
     """
@@ -76,7 +127,7 @@ def generate_barcodes(nIds, codes=None, idx=-1, codeLen=12):
     return list(set(codes))[:nIds]
     
 
-def write_mapping_file(mapFN, sampleIDs, barcodes):
+def write_mapping_file(mapF, sampleIDs, barcodes):
     """
     Given a mapping from sample IDs to barcodes/primers/other info, 
     write out a QIIME-compatible mapping file.
@@ -89,15 +140,16 @@ def write_mapping_file(mapFN, sampleIDs, barcodes):
     primer = 'GGGGGGGGGGGGGGGGGGGG'
     sampleMap = {}
     
-    with open(mapFN, 'w') as mapfile:
-        mapfile.write(header)
+    with mapF:
+        mapF.write(header)
         for sid, bc in zip(sampleIDs, barcodes):
             sampleMap[sid] = MapRecord(bc, primer, sid)
-            mapfile.write('\t'.join([sid, bc, primer, sid, '\n']))
+            mapF.write('\t'.join([sid, bc, primer, sid, '\n']))
     
     return sampleMap
 
-def scrobble_fasta_dir(dataDir, sampleMap, outF, sampleID=None):
+def scrobble_fasta_dir(dataDir, sampleMap, outF, sampleID=None, 
+                       idPattern=None, utf16=False):
     """
     Given a sample ID and a mapping, modify a Sanger FASTA file 
     to include the barcode and 'primer' in the sequence data 
@@ -106,11 +158,18 @@ def scrobble_fasta_dir(dataDir, sampleMap, outF, sampleID=None):
     for item in os.listdir(dataDir):
         if (osp.isfile(os.path.join(dataDir, item)) and 
             osp.splitext(item)[1] in file_types['fasta']):
-            for record in SeqIO.parse(os.path.join(dataDir, item), 'fasta'):
-                record.seq = ''.join(sampleMap[sampleID].barcode,
-                                     sampleMap[sampleID].primer, record.seq)
+            fh = open_enc(os.path.join(dataDir, item), utf16)
+            records = SeqIO.parse(fh, 'fasta')
+            for record in records:
+                if idPattern is not None:
+                    sep, field = idPattern
+                    sampleID = record.id.split(sep)[field - 1]
+                record.seq = (sampleMap[sampleID].barcode + 
+                              sampleMap[sampleID].primer + 
+                              record.seq)
                 SeqIO.write(record, outF, 'fasta')
-            
+            fh.close()
+
 
 def handle_program_options():
     """
@@ -127,12 +186,14 @@ def handle_program_options():
                                      barcodes and primers to the sequence \
                                      data, and outputting a mapping file and\
                                      single FASTA-formatted sequence file \
-                                     formed by concatenating all input data.") 
-                                     
-    parser.add_argument('input_dir', help="The directory containing sequence \
-                        data files. Assumes all data files are placed in this \
-                        directory. For files organized within folders by \
-                        sample, use -s in addition.")
+                                     formed by concatenating all input data.")
+    parser.add_argument('--version', action='version', 
+                        version='Sanger-QIIMify 0.1')
+    parser.add_argument('input_dir', 
+                        help="The directory containing sequence data files. \
+                              Assumes all data files are placed in this \
+                              directory. For files organized within folders by\
+                              sample, use -s in addition.")
     parser.add_argument('output_map_file', type=argparse.FileType('w'),
                         help="QIIME-formatted mapping file linking Sample IDs \
                               with barcodes and primers.")
@@ -143,35 +204,62 @@ def handle_program_options():
     parser.add_argument('-b', '--barcode_length', type=int, default=12,
                         help="Length of the generated barcode sequences. \
                               Default is 12 (QIIME default), minimum is 8.")
-    parser.add_argument('-i', '--identifier_pattern', type=tuple, default=(),
+    parser.add_argument('-i', '--identifier_pattern', action=ValidateIDPattern,
+                        nargs=2, metavar=('SEPARATOR', 'FIELD_NUMBER'),
                         help="Indicates how to extract the Sample ID from the \
                               description line. Specify two things: 1. Field \
-                              separator, 2. Field number of Sample ID. If the \
-                              separator is a space or tab, use \s or \\t \
-                              respectively. Example: >ka-SampleID-2091, use \
-                              -i - 2, indicating - is the separator and the \
-                              Sample ID is field #2.")
-    parser.add_argument('-s', '--subdirectories', 
+                              separator, 2. Field number of Sample ID (1 or \
+                              greater). If the separator is a space or tab, \
+                              use \s or \\t respectively. \
+                              Example: >ka-SampleID-2091, use -i - 2, \
+                              indicating - is the separator and the Sample ID \
+                              is field #2.")
+    parser.add_argument('-s', '--subdirectories',
                         help="Specify that sequence data files are contained \
                               in subfolders named by Sample ID.",
                         action='store_true')
-    return parser.parse_args(sys.argv)
+    # data input file type options
+    parser.add_argument('-q', '--qual', action='store_true', default=False,
+                        help="Instruct the program to look for quality \
+                              input files")
+    parser.add_argument('-u', '--utf16', action='store_true', default=False,
+                        help="UTF-16 encoded input files")
+    
+    return parser.parse_args(), parser
     
 
     
 def main():
-    args = handle_program_options()
-    pass
-#    sampleIDs, dirs = gather_hierarchical_sample_ids(sys.argv[1])
-#    barcodes = generate_barcodes(len(sampleIDs))
-#    sampleMap = write_mapping_file(sys.argv[2], sampleIDs, barcodes)
-#    outF = open(sys.argv[3], 'w')
-#    
-#    for dataDir, sampleID in zip(dirs,sampleIDs):
-#        scrobble_fasta_dir(dataDir, sampleMap, outF, sampleID)
-#    
-#    outF.close()
+    args, parser = handle_program_options()
+    
+    if args.subdirectories:
+        sampleIDs, dirs = gather_hierarchical_sample_ids(args.input_dir, 
+                                                         args.utf16)
+    else:
+        if args.identifier_pattern:
+            sampleIDs =  gather_sample_ids(args.input_dir, 
+                                           args.identifier_pattern, 
+                                           args.utf16)
+        else:
+            msg = ' '.join(['Error: Must specify identifier pattern:',
+                            '-i separator field_number'])
+            parser.exit(-1, msg)
 
+    barcodes = generate_barcodes(len(sampleIDs), codeLen=args.barcode_length)
+    sampleMap = write_mapping_file(args.output_map_file, sampleIDs, barcodes)
+
+    with args.output_sequence_file:
+        if args.subdirectories:
+            for dataDir, sampleID in zip(dirs,sampleIDs):
+                scrobble_fasta_dir(dataDir, sampleMap,
+                                   args.output_sequence_file, 
+                                   sampleID=sampleID, 
+                                   utf16=args.utf16)
+        else:
+            scrobble_fasta_dir(args.input_dir, sampleMap, 
+                               args.output_sequence_file, 
+                               idPattern=args.identifier_pattern,
+                                utf16=args.utf16)
 
 if __name__ == '__main__':
     main()
