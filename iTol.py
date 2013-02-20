bdd19b7ef4d2c14379829c0c73c1b22f31fa546f
Shareef Dabdoub <dabdoub.2@osu.edu>
2013-02-20 13:34:49 -0500

    Adds raw abundance calculation as an option for output. Refactors to provide multiple choices for calculation methods

--- iTol.py
+++ iTol.py
@@ -4,46 +4,25 @@ Created on Feb 8, 2012
 
 @author: Shareef M Dabdoub
 '''
+import biom_calc as bc
 import util
 import argparse
-from collections import defaultdict, namedtuple, OrderedDict
+from collections import namedtuple, OrderedDict
 import json
+
 
 def otu_name(biom_row):
     tax = biom_row['metadata']['taxonomy']
     for i, lvl in enumerate(tax):
+        lvl = lvl.strip()
         if i < len(tax) - 1 and len(tax[i + 1].strip()) == 3:
             return 'Unclassified_' + lvl.split('_')[-1]
         elif i == len(tax) - 1:
-            return lvl.split('_')[-1]
+            name = lvl.split('_')[-1]
+            if lvl[0] == 's':
+                name = tax[i-1].split('_')[-1] + '_' + name 
+            return name
 
-
-def relative_abundance(biom, filterIDs):
-    ra = {item['id']: defaultdict(int) for item in biom['columns'] 
-           if item['id'] in filterIDs}
-    totals = defaultdict(float)
-    
-    for row, col, amt in biom['data']:
-        oname = otu_name(biom['rows'][row])
-        sampleID = biom['columns'][col]['id']
-        
-        if sampleID in filterIDs:
-            ra[sampleID][oname] = amt
-            totals[sampleID] += amt
-    
-    return {sid: {oid: ra[sid][oid] / totals[sid] for oid in ra[sid]} 
-              for sid in ra}
-
-
-def mean_otu_pct_abundance(ra, otuids):
-    sids = ra.keys()
-    otumeans = defaultdict(int)
-    
-    for oid in otuids:
-        otumeans[oid] = sum([ra[sid][oid] for sid in sids 
-                               if oid in ra[sid]]) / len(sids)
-    
-    return otumeans
 
 def newick_replace_otuids(tree, biom):
     """
@@ -57,7 +36,7 @@ def newick_replace_otuids(tree, biom):
 
 # Meant to contain all the data necessary for calculating a single column of 
 # an iTol data table
-DataCategory = namedtuple('DataCategory', 'sids ra means')
+DataCategory = namedtuple('DataCategory', 'sids results')
 
 def gather_categories(imap, header, categories=None):
     """
@@ -81,7 +60,7 @@ def gather_categories(imap, header, categories=None):
     		  specified, a single entry with the key 'default' will be returned  
     """
     if categories is None:
-        return {'default': DataCategory(set(imap.keys()), {}, {})}
+        return {'default': DataCategory(set(imap.keys()), {})}
     
     cat_ids = [header.index(cat) for cat in categories if cat in header
                                                          and '=' not in cat]
@@ -90,15 +69,20 @@ def gather_categories(imap, header, categories=None):
         if '=' in cat and cat.split('=')[0] in header:
             conditions[header.index(cat.split('=')[0])] =  cat.split('=')[1]
 
-    if not cat_ids:
-        return {'default': DataCategory(set(imap.keys()), {}, {})}
+    if not cat_ids and not conditions:
+        return {'default': DataCategory(set(imap.keys()), {})}
+    
+    if not cat_ids and conditions:
+        sids = {sid for sid,row in imap.iteritems() if 
+                            all([row[c] == conditions[c] for c in conditions])}
+        return {'default': DataCategory(sids, {})}
     
     table = {}
     for sid,row in imap.iteritems():
         if all([row[c] == conditions[c] for c in conditions]):
             key = '_'.join([row[cid] for cid in cat_ids])
             if not key in table:
-                table[key] = DataCategory(set(), {}, {})
+                table[key] = DataCategory(set(), {})
             table[key].sids.add(sid)
             
     return OrderedDict(sorted(table.items(), key=lambda t: t[0]))
@@ -115,11 +99,12 @@ def handle_program_options():
                                      instead of OTU IDs for useful display in \
                                      iTol.")
     parser.add_argument('-i', '--otu_table', required=True,
-                        help="The biom file with OTU-Sample abundance data.")
+                        help="The biom-format file with OTU-Sample abundance \
+                              data.")
     parser.add_argument('-m', '--mapping', required=True,
                         help="The mapping file specifying group information \
                               for each sample.")
-    parser.add_argument('-t', '--input_tre', required=True, 
+    parser.add_argument('-t', '--input_tree', default='',
                         help="A phylogenetic tree in Newick format to be \
                               modified by exchanging the OTU ID node names for\
                               taxonomic names.")
@@ -128,14 +113,10 @@ def handle_program_options():
     parser.add_argument('-o', '--output_itol_table', default='iTol_table.txt',
                         help="Other than a phylogenetic tree, the main input \
                               to iTol is a dataset file containing some \
-                              representation of the relative abundance of \
-                              every OTU across the specified data groups. This\
-                              program calculates either the (MRE) mean \
-                              relative abundance (relative abundance of each \
-                              sample combined to find the mean across OTUs) or\
-                              the MRE normalized across the groups (the group \
-                              MRE of each OTU divided by the sum for that \
-                              OTU).")
+                              representation of the abundance of every OTU \
+                              across the specified data groups. This program \
+                              provides multiple calculation methods. See the \
+                              --analysis_metric option for details.")
     parser.add_argument('-c', '--map_categories', default=None,
                         help="Any mapping categories, such as treatment type, \
                               that will be used to group the data in the \
@@ -145,11 +126,18 @@ def handle_program_options():
                               three types each will result in six data \
                               columns. Default is no categories and all the \
                               data will be treated as a single group.")
-    parser.add_argument('-N', '--normalized_MRE', action='store_true', 
-                         help="Specifies whether the mean relative abundance \
-                                calculated for each OTU and group is \
-                                normalized by the sum for the whole OTU. \
-                                Default is False.")
+    parser.add_argument('-a', '--analysis_metric', default='MRE', 
+                        choices=['MRA','NMRA','raw'],
+                        help="Specifies which metric is calculated on the \
+                              abundance data in the OTU table. Available \
+                              options: MRE - mean relative abundance \
+                              (Abundance data is normalized by total sample \
+                              abundance, then averaged across OTU), NMRE - \
+                              normalized mean relative abundance (MRE \
+                              normalized by the total MRE across the groups \
+                              as specified in --map_categories), raw (outputs \
+                              the actual sequence abundance data for \
+                              each OTU).")
 #    parser.add_argument('-v', '--verbose', action='store_true')
     
     return parser.parse_args()
@@ -161,24 +149,38 @@ def main():
     # input data
     with open(args.otu_table) as bF:
         biom = json.loads(bF.readline())
-    with open(args.input_tre) as treF, open(args.output_tre, 'w') as outF:
-        outF.write(newick_replace_otuids(treF.readline(), biom))
     imap = util.parse_map_file(args.mapping)
     with open(args.mapping, 'rU') as mapF:
         map_header = mapF.readline()[1:].split()
+        
+    # rewrite tree file with otu names
+    if args.input_tree:
+        with open(args.input_tree) as treF, open(args.output_tre, 'w') as outF:
+            outF.write(newick_replace_otuids(treF.readline(), biom))
     
-    groups = gather_categories(imap, map_header, 
-                               args.map_categories.split(','))
-    all_otus = {otu_name(item) for item in biom['rows']}
-
+    oid_rows = {row['id']:row for row in biom['rows']}
+    
+    # calculate analysis results
+    categories = None
+    if args.map_categories is not None:
+        categories = args.map_categories.split(',')
+    
+    groups = gather_categories(imap, map_header, categories)
     for group in groups.values():
-        group.ra.update(relative_abundance(biom, group.sids))
-        group.means.update(mean_otu_pct_abundance(group.ra, all_otus))
-    
+        if args.analysis_metric in ['MRA','NMRA']:
+            results = bc.MRA(biom, group.sids)
+        elif args.analysis_metric == 'raw':
+            results = bc.raw_abundance(biom, group.sids)
+
+        group.results.update({otu_name(oid_rows[oid]):results[oid] 
+                                    for oid in results})
+        
+    # write iTol data set file
     with open('iTol_table.txt', 'w') as itolF:
         itolF.write('LABELS\t' + '\t'.join(groups.keys())+'\n')
         itolF.write('COLORS\t{}\n'.format('\t'.join(['#ff0000' 
 											    for _ in range(len(groups))])))
+        all_otus = frozenset({otu_name(row) for row in biom['rows']})
         
         for oname in all_otus:
             row = ['{name}']#\t{s:.2f}\t{ns:.2f}\n'
@@ -186,13 +188,13 @@ def main():
             msum = 0
             for name, group in groups.iteritems():
                 row.append('{{{}:.5f}}'.format(name))
-                if oname in group.means:
-                    row_data[name] = group.means[oname] * 100 
+                if oname in group.results:
+                    row_data[name] = group.results[oname] 
                 else:
                     row_data[name] = 0.0
                 msum += row_data[name]
             # normalize avg relative abundance data
-            if args.normalized_MRE and msum > 0:
+            if args.analysis_metric == 'NMRA' and msum > 0:
                 row_data.update({key:data/msum for key,data in row_data.items() 
                                    if key != 'name'})
             
