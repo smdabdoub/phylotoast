c111763aaddf57cdf0c1d2f4582226e6a54c9e37
Shareef Dabdoub <dabdoub.2@osu.edu>
2014-09-18 14:48:15 -0400

    Adds title, version, author, license, copyright magic variables to project __init__.py

---
+++ qiime_tools/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'qiime-tools'
+__version__ = '1.1.2'
+__author__ = 'Shareef M Dabdoub'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Shareef M Dabdoub'
