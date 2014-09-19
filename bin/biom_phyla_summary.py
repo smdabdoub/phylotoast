e2c5ba13578c9b42272dbf7ae7e4244a774f729e
Shareef Dabdoub <dabdoub.2@osu.edu>
2014-09-19 10:56:28 -0400

    Converts biom_phyla_summary to using argparse, separates out code into defined functionality.

--- bin/biom_phyla_summary.py
+++ bin/biom_phyla_summary.py
@@ -1,8 +1,33 @@
-import sys
+#!/usr/bin/env python
+'''
+This is a quick and dirty script that fits a need I have, 
+but should be taken more as an example to build on for 
+your specific needs.
+
+@author: Shareef M Dabdoub
+'''
+import argparse
 import json
 from collections import defaultdict
 
-def biom_summary(biom):
+
+def levelData(counts, seqCount, level='g'):
+    lvlData = sorted([(k, counts[k]) for k in counts if level+'_' in k], key=lambda x: x[1], reverse=True)
+    return ['{}: {:4.2f}%'.format(p[0], p[1]/seqCount*100) for p in lvlData]
+    
+    
+def lookup(s, d):
+    for idx, item in enumerate(d):
+        if s in item:
+            return d[idx]
+    return s+': None'
+
+
+def summarize_taxa(biom):
+    """
+    Given an abundance table, group the counts by every 
+    taxonomic level.
+    """
     tamtcounts = defaultdict(int)
     tot_seqs = 0.0
     
@@ -13,67 +38,83 @@ def biom_summary(biom):
             t = t.strip()
             if i == len(rtax)-1 and len(t) > 3 and len(rtax[-1]) > 3:
                 t = 's__'+rtax[i-1].strip().split('_')[-1]+'_'+t.split('_')[-1]
-            tamtcounts[t] += amt        
+            tamtcounts[t] += amt
+            
+    lvlData = {lvl: levelData(tamtcounts, tot_seqs, lvl) for lvl in ['k', 'p', 'c', 'o', 'f', 'g', 's']}
+            
+    return tot_seqs, lvlData
     
+
+def general_taxon_summary(lvlData, tot_seqs):
     print 'Total sequence count: %i\n' % tot_seqs
-    
-    def levelData(counts, seqCount, level='g'):
-        lvlData = sorted([(k, counts[k]) for k in counts if level+'_' in k], key=lambda x: x[1], reverse=True)
-        return ['{}: {:4.2f}%'.format(p[0], p[1]/seqCount*100) for p in lvlData]
-    
-    def lookup(s, d):
-        for idx, item in enumerate(d):
-            if s in item:
-                return d[idx]
-        return s+': None'
-    
+
     print 'Top families:\n'
-    lvlDataF = levelData(tamtcounts, tot_seqs, 'f')
-    print lvlDataF[:20]
-            
+    print lvlData['f'][:20]
+        
     print '\nTop genera:\n'
-    lvlDataG = levelData(tamtcounts, tot_seqs)
-    print lvlDataG[:20]
-            
+    print lvlData['g'][:20]
+        
     print '\nTop species:\n'
-    lvlData = levelData(tamtcounts, tot_seqs, 's')
-    print lvlData[:20]
+    print lvlData['s'][:20]
     
-    
+
+def oral_taxon_summary(lvlData):
     print '\nRed Complex:'
-    print lookup('s__Porphyromonas_gingivalis', lvlData)
-    print lookup('s__Treponema_denticola', lvlData)
-    print lookup('s__Tannerella_forsythia', lvlData)
-    
+    print lookup('s__Porphyromonas_gingivalis', lvlData['s'])
+    print lookup('s__Treponema_denticola', lvlData['s'])
+    print lookup('s__Tannerella_forsythia', lvlData['s'])
+
     print '\nOrange Complex: '
-    print lookup('s__Fusobacterium_nucleatum', lvlData)
-    print lookup('s__Prevotella_intermedia', lvlData)
-    print lookup('s__Prevotella_nigrescens', lvlData)
-    print lookup('s__Peptostreptococcus_micros', lvlData)
-    
+    print lookup('s__Fusobacterium_nucleatum', lvlData['s'])
+    print lookup('s__Prevotella_intermedia', lvlData['s'])
+    print lookup('s__Prevotella_nigrescens', lvlData['s'])
+    print lookup('s__Peptostreptococcus_micros', lvlData['s'])
+
     print '\nYellow Complex: '
-    print lookup('s__Streptococcus_sanguis', lvlData)
-    print lookup('s__Streptococcus_oralis', lvlData)
-    print lookup('s__Streptococcus_mitis', lvlData)
-    print lookup('s__Streptococcus_gordonii', lvlData)
-    print lookup('s__Streptococcus_intermedius', lvlData)
-    
+    print lookup('s__Streptococcus_sanguis', lvlData['s'])
+    print lookup('s__Streptococcus_oralis', lvlData['s'])
+    print lookup('s__Streptococcus_mitis', lvlData['s'])
+    print lookup('s__Streptococcus_gordonii', lvlData['s'])
+    print lookup('s__Streptococcus_intermedius', lvlData['s'])
+
     print '\nGreen Complex: '
-    print lookup('s__Aggregatibacter_actinomycetemcomitans', lvlData)
-    print lookup('s__Campylobacter_concisus', lvlData)
-    print lookup('s__Eikenella_corrodens', lvlData)
-    print lookup('g__Capnocytophaga', lvlDataG)
-    
+    print lookup('s__Aggregatibacter_actinomycetemcomitans', lvlData['s'])
+    print lookup('s__Campylobacter_concisus', lvlData['s'])
+    print lookup('s__Eikenella_corrodens', lvlData['s'])
+    print lookup('g__Capnocytophaga', lvlData['g'])
+
     print '\nPurple Complex: '
-    print lookup('s__Veillonella_parvula', lvlData)
-    print lookup('s__Actinomyces_odontolyticus', lvlData)
-    print lookup('s__Selenomonas_noxia', lvlData)
-    print lookup('s__Actinomyces_naeslundii', lvlData)
+    print lookup('s__Veillonella_parvula', lvlData['s'])
+    print lookup('s__Actinomyces_odontolyticus', lvlData['s'])
+    print lookup('s__Selenomonas_noxia', lvlData['s'])
+    print lookup('s__Actinomyces_naeslundii', lvlData['s'])
+
+
+def handle_program_options():
+    parser = argparse.ArgumentParser(description="Print a taxonomic summary of \
+                                     a given BIOM abundance table.")
+    parser.add_argument('-i', '--otu_table', required=True,
+                        help="The biom-format file with OTU-Sample abundance \
+                              data.")
+    parser.add_argument('--oral_taxa', action='store_true',
+                        help="If specified, print an additional summary of taxa \
+                              important to the oral microbiome (color complexes).")
+    
+    return parser.parse_args()
+
 
 def main():
-    with open(sys.argv[1], 'rU') as bF:
-        biom = json.loads(bF.readline())
-        biom_summary(biom)
+    args = handle_program_options()
+    with open(args.otu_table, 'rU') as bF:
+        biom = json.load(bF)
+        tot_seqs, lvlData = summarize_taxa(biom)
+        
+        # print summaries
+        general_taxon_summary(lvlData, tot_seqs)
+        
+        if args.oral_taxa:
+            oral_taxon_summary(lvlData)
+        
     
 if __name__ == '__main__':
     main()
