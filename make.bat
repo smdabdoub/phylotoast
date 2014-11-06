6e5c472dae37aaf98c78bbb457fcd19446affa0c
Akshay Paropkari <paropkari.1@gmail.com>
2014-11-06 10:37:15 -0500

    Updated documentation source files
    Added auto-generated API source files and changed other source files to
    txt files.

--- make.bat
+++ make.bat
@@ -115,9 +115,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\QIIME-Tools.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\qiime-tools.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\QIIME-Tools.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\qiime-tools.ghc
 	goto end
 )
 
