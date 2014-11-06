6c37fe39414345871d8ec6876099baaac355853d
Akshay Paropkari <paropkari.1@gmail.com>
2014-11-06 14:34:04 -0500

    Updates to test scripts
    Deleted email contact from docstring. Updated otu_calc to use tempfile
    module.

--- qiime_tools/test/test_otu_calc.py
+++ qiime_tools/test/test_otu_calc.py
@@ -2,14 +2,13 @@
 """
 :Author: Akshay Paropkari
 
-:Contact: paropkari.1@gmail.com
-
 :Date Created: 10/22/2014
 
 :Abstract: Automated Tests for OTU calculations.
 """
 import unittest
 import json
+import os
 import tempfile
 import sys
 from qiime_tools import otu_calc as oc
@@ -88,6 +87,26 @@ class otu_calc_Test(unittest.TestCase):
                              [4,2,1]
                             ]
                         }"""
+
+        self.coreOTUdata = '''# Core OTUs across 75 % of samples.
+064EK048	[u'k__Bacteria', u'p__Firmicutes', u'c__Bacilli', u'o__Lactobacillales', u'f__Streptococcaceae', u'g__Streptococcus', u's__HOT.064']
+200CZ006	[u'k__Bacteria', u'p__Fusobacteria', u'c__Fusobacteria', u'o__Fusobacteriales', u'f__Fusobacteriaceae', u'g__Fusobacterium', u's__nucleatum']
+161Vparv	[u'k__Bacteria', u'p__Firmicutes', u'c__Clostridia', u'o__Clostridiales', u'f__Veillonellaceae', u'g__Veillonella', u's__parvula']
+111_3523	[u'k__Bacteria', u'p__Firmicutes', u'c__Clostridia', u'o__Clostridiales', u'f__Peptostreptococcaceae_[XIII]', u'g__Parvimonas', u's__micra']
+644_4671	[u'k__Bacteria', u'p__Firmicutes', u'c__Bacilli', u'o__Lactobacillales', u'f__Streptococcaceae', u'g__Streptococcus', u's__intermedius']
+758_3928	[u'k__Bacteria', u'p__Firmicutes', u'c__Bacilli', u'o__Lactobacillales', u'f__Streptococcaceae', u'g__Streptococcus', u's__sanguinis']
+524Vatyp	[u'k__Bacteria', u'p__Firmicutes', u'c__Clostridia', u'o__Clostridiales', u'f__Veillonellaceae', u'g__Veillonella', u's__atypica']
+151_K168	[u'k__Bacteria', u'p__Firmicutes', u'c__Clostridia', u'o__Clostridiales', u'f__Veillonellaceae', u'g__Selenomonas', u's__sputigena']
+530_5256	[u'k__Bacteria', u'p__Actinobacteria', u'c__Actinobacteria', u'o__Actinomycetales', u'f__Propionibacteriaceae', u'g__Propionibacterium', u's__acnes']
+612_6662	[u'k__Bacteria', u'p__Proteobacteria', u'c__Gammaproteobacteria', u'o__Pseudomonadales', u'f__Pseudomonadaceae', u'g__Pseudomonas', u's__fluorescens']
+539_6962	[u'k__Bacteria', u'p__Firmicutes', u'c__Clostridia', u'o__Clostridiales', u'f__Peptostreptococcaceae_[XI]', u'g__Filifactor', u's__alocis']
+073-T1E5	[u'k__Bacteria', u'p__Firmicutes', u'c__Bacilli', u'o__Lactobacillales', u'f__Streptococcaceae', u'g__Streptococcus', u's__australis']
+398_7051	[u'k__Bacteria', u'p__Firmicutes', u'c__Bacilli', u'o__Lactobacillales', u'f__Streptococcaceae', u'g__Streptococcus', u's__mitis']
+622_3931	[u'k__Bacteria', u'p__Firmicutes', u'c__Bacilli', u'o__Lactobacillales', u'f__Streptococcaceae', u'g__Streptococcus', u's__gordonii']
+826_8612	[u'k__Bacteria', u'p__Proteobacteria', u'c__Gammaproteobacteria', u'o__Pasteurellales', u'f__Pasteurellaceae', u'g__Terrahaemophilus', u's__aromaticivorans']
+623_4320	[u'k__Bacteria', u'p__Proteobacteria', u'c__Epsilonproteobacteria', u'o__Campylobacterales', u'f__Campylobacteraceae', u'g__Campylobacter', u's__gracilis']
+205FL002	[u'k__Bacteria', u'p__Fusobacteria', u'c__Fusobacteria', u'o__Fusobacteriales', u'f__Fusobacteriaceae', u'g__Fusobacterium', u's__HOT.205']'''
+
         self.biom = json.loads(self.biom_text)
         self.row = self.biom['rows']
 
@@ -95,7 +114,7 @@ class otu_calc_Test(unittest.TestCase):
         """
         Testing fuzzy_lookup() function of otu_calc.py.
 
-        :return:Returns OK if the test goals were achieved, otherwise
+        :return: Returns OK if the test goals were achieved, otherwise
                 raises error.
         """
         self.result1 = oc.assign_otu_membership(self.biom)['Sample2']
@@ -114,8 +133,8 @@ class otu_calc_Test(unittest.TestCase):
         """
         Testing sdi() function of otu_calc.py.
 
-        :return:Returns OK if the test goals were achieved, otherwise
-                raises error.
+        :return: Returns OK if the test goals were achieved, otherwise
+                 raises error.
         """
         self.result1 = oc.assign_otu_membership(self.biom)
         fset = self.result1['Sample4']
@@ -134,8 +153,8 @@ class otu_calc_Test(unittest.TestCase):
         """
         Testing otu_name_biom() function of otu_calc.py.
 
-        :return:Returns OK if the test goals were achieved, otherwise
-                raises error.
+        :return: Returns OK if the test goals were achieved, otherwise
+                 raises error.
         """
         self.result = oc.otu_name_biom(self.row[3])
         hand_calc = 'Halanaerobium_Halanaerobiumsaccharolyticum'
@@ -150,8 +169,8 @@ class otu_calc_Test(unittest.TestCase):
         """
         Testing otu_name() function of otu_calc.py.
 
-        :return:Returns OK if the test goals were achieved, otherwise
-                raises error.
+        :return: Returns OK if the test goals were achieved, otherwise
+                 raises error.
         """
         self.tax = [
             "k__Archaea", "p__Euryarchaeota", "c__Methanomicrobia",
@@ -171,13 +190,14 @@ class otu_calc_Test(unittest.TestCase):
         """
         Testing load_core_file() function of otu_calc.py
 
-        :return:Returns OK if the test goals were achieved, otherwise
-                raises error.
+        :return: Returns OK if the test goals were achieved, otherwise
+                 raises error.
         """
-        self.result = oc.load_core_file(
-            '/Users/akshay/dev/core_otus_example.txt'
-            )
-        length = len(self.result)
+        temp_file1 = tempfile.NamedTemporaryFile(delete=False)
+        temp_file1.write(self.coreOTUdata)
+        temp_file1.seek(0)
+        result = oc.load_core_file(temp_file1.name)
+        length = len(result)
 
         # Testing if all core OTU's samples were in the output.
         self.assertEqual(
@@ -185,12 +205,15 @@ class otu_calc_Test(unittest.TestCase):
             msg='Error! Output does not contain all core OTU in samples.'
         )
 
+        # Deleting temporary file.
+        os.unlink(temp_file1.name)
+
     def test_assign_otu_membership(self):
         """
         Testing assign_otu_membership() function of otu_calc.py.
 
         :return: Returns OK if the test goals were achieved, otherwise
-                raises error.
+                 raises error.
         """
         self.result = oc.assign_otu_membership(self.biom)
 
@@ -209,24 +232,25 @@ class otu_calc_Test(unittest.TestCase):
     def test_print_membership(self):
         """
         Testing print_membership() function of otu_calc.py. Output of this
-        function is written to output.txt file. The contents of output.txt
+        function is written to a temporary file. The contents of the temporary
         file are accessed and used to compare with expected output.
 
-        :return:Returns OK if the test goals were achieved, otherwise
-                raises error.
+        :return: Returns OK if the test goals were achieved, otherwise
+                 raises error.
         """
         self.result1 = oc.assign_otu_membership(self.biom)
 
-        # Writing the output of print_membership() to output.txt file
+        # Writing the output of print_membership() to temporary file.
         saveout = sys.stdout
-        with open('/Users/akshay/Desktop/docs/output.txt', 'w') as f:
+        temp_file2 = tempfile.NamedTemporaryFile(delete=False)
+        with open(temp_file2.name, 'w') as f:
             sys.stdout = f
             self.result = oc.print_membership(self.result1['Sample1'])
         sys.stdout = saveout
 
         # Obtaining the function output from output.txt file.
         func_calc = []
-        with open('/Users/akshay/Desktop/docs/output.txt', 'r') as out:
+        with open(temp_file2.name, 'r') as out:
             while True:
                 statement = out.readline()
                 func_calc.append(statement)
@@ -245,14 +269,14 @@ class otu_calc_Test(unittest.TestCase):
             msg='Error! Output does not print out the right information correctly.'
         )
 
+        # Deleting temporary file.
+        os .unlink(temp_file2.name)
+
     def tearDown(self):
         """
         Tearing down of this unittest framework.
         """
-        # Erasing data from output.txt file used in testing print_membership
-        # function.
-        with open('/Users/akshay/Desktop/docs/output.txt', 'w') as f:
-            f.write('')
+        pass
 
 if __name__ == '__main__':
     unittest.main()
