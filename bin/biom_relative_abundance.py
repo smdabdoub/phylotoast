f2703d2073379fa955fb25bb78fcf1fae23554fe
Akshay Paropkari <paropkari.1@gmail.com>
2016-01-04 16:50:41 -0500

    Updated biom_relative_abundance.py
    Added the ability to work with any type of biom file.
    [ci skip]

--- bin/biom_relative_abundance.py
+++ bin/biom_relative_abundance.py
@@ -5,19 +5,19 @@ each OTU and write out to a tab-separated file listing OTUs as
 rows and Samples as columns.
 """
 from __future__ import division
-import argparse
-import json
 import sys
+import biom
+import argparse
 from phylotoast import biom_calc as bc
 from phylotoast import otu_calc as oc
 
 
-def write_relative_abundance(rel_abd, biom, out_fn, sort_by=None):
+def write_relative_abundance(rel_abd, biomf, out_fn, sort_by=None):
     """
     Given a BIOM table, calculate per-sample relative abundance for
     each OTU and write out to a tab-separated file listing OTUs as
     rows and Samples as columns.
-    :type biom: dict (translated json string)
+    :type biom: biom object
     :param biom: BIOM-formatted OTU/Sample abundance data
     :type out_fn: str
     :param out_fn: The full path to the desired output file.
@@ -27,13 +27,15 @@ def write_relative_abundance(rel_abd, biom, out_fn, sort_by=None):
                      the output file.
     """
     with open(out_fn, 'w') as out_f:
-        sids = sorted(set([col['id'] for col in biom['columns']]), key=sort_by)
+        sids = sorted(set(biomf.ids()), key=sort_by)
         out_f.write('#OTU ID\t{}\n'.format('\t'.join(sids)))
 
-        for row in biom['rows']:
-            otuName = oc.otu_name_biom(row)
-            otuid = row['id']
-            sabd = [str(rel_abd[sid][otuid]) if sid in rel_abd and otuid in rel_abd[sid] else '0' for sid in sids]
+        for otuid in biomf.ids(axis="observation"):
+            otuName = oc.otu_name(biomf.metadata(otuid, "observation")\
+                      ["taxonomy"])
+            sabd = [str(rel_abd[sid][otuid]) 
+                    if sid in rel_abd and otuid in rel_abd[sid] else '0' 
+                    for sid in sids]
             out_f.write('{}\t{}\n'.format(otuName, '\t'.join(sabd)))
 
 
@@ -42,7 +44,7 @@ def handle_program_options():
                                                   data into a TSV file of relative \
                                                   abundance data.")
     parser.add_argument('-i', '--input_biom_fp',
-                        help="The BIOM file path.")
+                        help="BIOM file path.")
     parser.add_argument('-o', '--output_tsv_fp', default='relative_abundance.tsv',
                         help="A TSV table of relative OTU abundance data.")
     parser.add_argument('--stabilize_variance', action='store_true',
@@ -61,13 +63,14 @@ def main():
     except IOError as ioe:
         sys.exit('\nError in BIOM file path: {}\n'.format(ioe))
 
-    with open(args.input_biom_fp, 'rU') as in_f:
-        biom = json.load(in_f)
-        rel_abd = bc.relative_abundance(biom)
-        if args.stabilize_variance:
-            rel_abd = bc.arcsine_sqrt_transform(rel_abd)
-        
-        write_relative_abundance(rel_abd, biom, args.output_tsv_fp)
+#     with open(args.input_biom_fp, 'rU') as in_f:
+    biomf = biom.load_table(args.input_biom_fp)
+    norm_biomf = biomf.norm(axis="observation", inplace=False)
+    rel_abd = bc.relative_abundance(norm_biomf)
+    if args.stabilize_variance:
+        rel_abd = bc.arcsine_sqrt_transform(rel_abd)
+
+    write_relative_abundance(rel_abd, norm_biomf, args.output_tsv_fp)
 
 if __name__ == '__main__':
     main()
