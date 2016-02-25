1a75c968606ce82dc3a60883632165ea53ade70e
Shareef Dabdoub <shareef@dabdoub.net>
2016-02-25 15:18:05 -0500

    Updates version to 1.3.0 in phylotoast/__init__.py

--- phylotoast/__init__.py
+++ phylotoast/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'phylotoast'
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __author__ = 'Shareef M Dabdoub'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Shareef M Dabdoub'
