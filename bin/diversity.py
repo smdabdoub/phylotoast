161ecc62ccb4e5eb23787aa5cfcac04f737c55da
Akshay Paropkari <paropkari.1@gmail.com>
2016-03-16 10:45:30 -0700

    Updated import error checking fir diversity.py and modified quotation marks for consistency.

--- bin/diversity.py
+++ bin/diversity.py
@@ -15,27 +15,27 @@ import os.path as osp
 importerrors = []
 try:
     import biom
-except ImportError as ie1:
-    importerrors.append(ie1)
+except ImportError as ie:
+    importerrors.append(ie)
 try:
     import scipy.stats as stats
-except ImportError as ie2:
-    importerrors.append(ie2)
+except ImportError as ie:
+    importerrors.append(ie)
 try:
     from skbio.diversity import alpha, beta
-except ImportError as ie3:
-    importerrors.append(ie3)
+except ImportError as ie:
+    importerrors.append(ie)
 try:
     import matplotlib
-except ImportError as ie4:
-    importerrors.append(ie4)
+except ImportError as ie:
+    importerrors.append(ie)
 try:
     from brewer2mpl import qualitative
-except ImportError as ie5:
-    importerrors.append(ie5)
+except ImportError as ie:
+    importerrors.append(ie)
 if len(importerrors) != 0:
     for item in importerrors:
-        print 'Import Error:', item
+        print "Import Error:", item
     sys.exit()
 
 
@@ -117,12 +117,12 @@ def plot_group_diversity(diversities, grp_colors, title, diversity_type, out_dir
         gu.plot_kde(diversities[grp], ax_div, title, grp_colors[grp])
 
     ax_div.set_xlabel(diversity_type)
-    ax_div.set_ylabel('Density')
+    ax_div.set_ylabel("Density")
     ax_div.legend([plt.Rectangle((0, 0), 1, 1, fc=color) for color in grp_colors.values()],
-                  grp_colors.keys(), loc='best')
+                  grp_colors.keys(), loc="best")
 
-    fig_div.savefig(osp.join(out_dir, diversity_type+'.'+plot_ext), facecolor='white',
-                    edgecolor='none', bbox_inches='tight', pad_inches=0.5)
+    fig_div.savefig(osp.join(out_dir, diversity_type+"."+plot_ext), facecolor="white",
+                    edgecolor="none", bbox_inches="tight", pad_inches=0.5)
 
 def write_diversity_metrics(data, sample_ids, fp=None):
     """
@@ -132,9 +132,9 @@ def write_diversity_metrics(data, sample_ids, fp=None):
     if fp is None:
         fp = "./diversity_data.txt"
 
-    with open(fp, 'w') as outf:
-        out = csv.writer(outf, delimiter='\t')
-        out.writerow(['calculation', 'group'])
+    with open(fp, "w") as outf:
+        out = csv.writer(outf, delimiter="\t")
+        out.writerow(["calculation", "group"])
         for group in data:
             for entry in data[group]:
                 out.writerow([entry, group])
@@ -147,37 +147,37 @@ def handle_program_options():
                                      metrics and output a kernal density \
                                      estimator-smoothed histogram of the \
                                      results.")
-    parser.add_argument('-m', '--map_file',
+    parser.add_argument("-m", "--map_file",
                         help="QIIME mapping file.")
-    parser.add_argument('-i', '--biom_fp',
+    parser.add_argument("-i", "--biom_fp",
                         help="Path to the BIOM table")
-    parser.add_argument('-c', '--category',
+    parser.add_argument("-c", "--category",
                         help="Specific category from the mapping file.")
-    parser.add_argument('-d', '--diversity', default=['shannon'], nargs='+',
-                        help='The alpha diversity metric. Default \
-                             value is "shannon", which will calculate the Shannon\
+    parser.add_argument("-d", "--diversity", default=["shannon"], nargs="+",
+                        help="The alpha diversity metric. Default \
+                             value is 'shannon', which will calculate the Shannon\
                              entropy. Multiple metrics can be specified (space separated).\
                              The full list of metrics is available at:\
                              http://scikit-bio.org/docs/latest/generated/skbio.diversity.alpha.html.\
-                             Beta diversity metrics will be supported in the future.')
-    parser.add_argument('--color_by',
+                             Beta diversity metrics will be supported in the future.")
+    parser.add_argument("--color_by",
                         help="A column name in the mapping file containing\
                               hexadecimal (#FF0000) color values that will\
                               be used to color the groups. Each sample ID must\
                               have a color entry.")
-    parser.add_argument('--plot_title', default='',
+    parser.add_argument("--plot_title", default="",
                         help="A descriptive title that will appear at the top \
                         of the output plot. Surround with quotes if there are\
                         spaces in the title.")
-    parser.add_argument('--x_label', default='diversity', nargs='+',
+    parser.add_argument("--x_label", default="diversity", nargs="+",
                         help="The name of the diversity metric to be displayed on the\
                         plot as the X-axis label. If multiple metrics are specified,\
                         then multiple entries for the X-axis label should be given.")
-    parser.add_argument('-o', '--out_dir', default=".",
+    parser.add_argument("-o", "--out_dir", default=".",
                         help="The directory plots will be saved to.")
-    parser.add_argument('--image_type', default='png',
+    parser.add_argument("--image_type", default="png",
                         help="The type of image to save: PNG, SVG, PDF, EPS, etc...")
-    parser.add_argument('--save_calculations',
+    parser.add_argument("--save_calculations",
                         help="A text file containing the calculated metrics will be stored\
                         to the file system. Default: 'diversity_data.txt'")
     return parser.parse_args()
@@ -190,20 +190,20 @@ def main():
         with open(args.map_file):
             pass
     except IOError as ioe:
-            err_msg = '\nError opening QIIME mapping file: {}\n'
+            err_msg = "\nError opening QIIME mapping file: {}\n"
             sys.exit(err_msg.format(ioe))
 
     try:
         with open(args.biom_fp):
             pass
     except IOError as ioe:
-            err_msg = '\nError opening BIOM table file: {}\n'
+            err_msg = "\nError opening BIOM table file: {}\n"
             sys.exit(err_msg.format(ioe))
 
     header, sample_map = putil.parse_map_file(args.map_file)
     biom_tbl = biom.load_table(args.biom_fp)
     if args.category not in header:
-        sys.exit('Category \'{}\' not found'.format(args.category))
+        sys.exit("Category '{}' not found".format(args.category))
 
     cat_idx = header.index(args.category)
     cat_vals = {entry[cat_idx] for entry in sample_map.values()}
@@ -216,7 +216,7 @@ def main():
     for method, x_label in zip(args.diversity, args.x_label):
         if method not in alpha.__all__:
             sys.exit("ERROR: Diversity metric not found: " + method)
-        metric = eval('alpha.'+method)
+        metric = eval("alpha."+method)
         div_calc, sample_ids = calc_diversity(metric, sample_map, biom_tbl, cat_vals, cat_idx)
 
         plot_group_diversity(div_calc, colors, plot_title, x_label,
@@ -231,23 +231,9 @@ def main():
         print
 
         if args.save_calculations:
-            prefix = '_'.join(x_label.split())
+            prefix = "_".join(x_label.split())
             write_diversity_metrics(div_calc, sample_ids, osp.join(args.out_dir, args.save_calculations))
 
 
-    # # Chao1 Diversity
-    # if args.diversity is None or 'chao1' in args.diversity:
-    #     plot_group_diversity(chao1, colors, 'Chao1 Diversity - '+plot_title,
-    #                          'Chao1 Diversity', args.out_dir, args.image_type)
-
-    # # Jaccard Similarity
-    # if args.diversity is None or 'jaccard' in args.diversity:
-    #     jaccard = beta.pw_distances_from_table(biom.load_table(args.biom_fp),
-    #                                            metric='jaccard')
-    #     fig_jaccard = jaccard.plot(title='Jaccard similarity heatmap - '+plot_title)
-    #     fig_jaccard.savefig(osp.join(args.out_dir, 'jaccard.'+args.image_type),
-    #                         facecolor='white', edgecolor='none')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
