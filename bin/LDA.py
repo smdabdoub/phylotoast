9f54d489fe1c8a35b037a0d0f84fda8c5d5ddae3
Akshay Paropkari <paropkari.1@gmail.com>
2016-04-27 15:17:16 -0400

    Updated LDA.py. Added functionality to calculate percent explained variance for SVD solver. Also added ability to write out explained variances as axis labels.

--- bin/LDA.py
+++ bin/LDA.py
@@ -37,7 +37,8 @@ def merge_dicts(*dict_args):
     return result
 
 
-def plot_LDA(X_lda, y_lda, class_colors, out_fp="", dpi=300, title=None):
+def plot_LDA(X_lda, y_lda, class_colors, exp_var,
+             out_fp="", dpi=300, title=None):
     cats = class_colors.keys()
     group_lda = {c: [] for c in cats}
     fig = plt.figure(figsize=(15, 10))
@@ -52,20 +53,21 @@ def plot_LDA(X_lda, y_lda, class_colors, out_fp="", dpi=300, title=None):
         plt.scatter(x=cat_x, y=cat_y, label=target_name,
                     color=class_colors[target_name],
                     alpha=0.85, s=250, edgecolors="k")
-    font = {"size": 16}
-    mpl.rc("font", **font)
+    mpl.rc("font", family="Arial") # define font for figure text
+    mpl.rc('xtick', labelsize=12)  # increase X axis ticksize
+    mpl.rc('ytick', labelsize=12)  # increase Y axis ticksize
     if X_lda.shape[1] == 1:
         plt.ylim((0.5, 2.5))
-    plt.xlabel("Linear Discriminant 1")
-    plt.ylabel("Linear Discriminant 2")
-    plt.legend(loc="best")
+    plt.xlabel("LD1 (Percent Explained Variance: {:.3f}%)".format(exp_var[0]*100), fontsize=16)
+    plt.ylabel("LD2 (Percent Explained Variance: {:.3f}%)".format(exp_var[1]*100), fontsize=16)
+    leg = plt.legend(loc="best", frameon=True, framealpha=1, fontsize=16)
+    leg.get_frame().set_edgecolor('k')
     if title:
         plt.title(title)
 
     # save or display result
     if out_fp:
-        plt.savefig(out_fp, facecolor="white",
-                    edgecolor="none", dpi=dpi,
+        plt.savefig(out_fp, facecolor="white", edgecolor="none", dpi=dpi,
                     bbox_inches="tight", pad_inches=0.1)
     else:
         plt.show()
@@ -77,10 +79,11 @@ def run_LDA(df):
     y = df["Condition"].values               # data categories list
 
     # Calculate LDA
-    sklearn_lda = LDA(n_components=2)
+    sklearn_lda = LDA()
     X_lda_sklearn = sklearn_lda.fit_transform(X, y)
+    exp_var = sklearn_lda.explained_variance_ratio_
 
-    return X_lda_sklearn, y
+    return X_lda_sklearn, y, exp_var
 
 
 def handle_program_options():
@@ -153,10 +156,10 @@ def main():
     if args.save_lda_input:
         df.to_csv(args.save_lda_input)
 
-    X_lda, y_lda = run_LDA(df)
+    X_lda, y_lda, exp_var = run_LDA(df)
 
-    plot_LDA(X_lda, y_lda, class_colors, out_fp=args.out_fp, dpi=args.dpi,
-             title=args.plot_title)
+    plot_LDA(X_lda, y_lda, class_colors, exp_var, out_fp=args.out_fp,
+             dpi=args.dpi, title=args.plot_title)
 
 
 if __name__ == "__main__":
