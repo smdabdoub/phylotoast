6c93822013744fc2f2c639535c63a512629a6a3d
Akshay Paropkari <paropkari.1@gmail.com>
2016-05-06 16:53:21 -0400

    Updated PCoA_bubble.py. Updated imports and relative abundance calculations for bubble sizes with improved biom handling. Updated plot_PCoA() method to account for optional  R style plot. Added ability (, ) to extract group and color information from mapping file. The input data file now takes in a list of OTU name, one per line, to plot. Added optional parameter () to save plots in different formats.

--- bin/PCoA_bubble.py
+++ bin/PCoA_bubble.py
@@ -1,59 +1,44 @@
 #!/usr/bin/env python
-'''
+"""
 Create a series of PCoA plots where the marker size varies by relative
 abundance of a particular OTU
 
 Author: Shareef M Dabdoub
-'''
+"""
 from __future__ import division
-# built-in
-import argparse
-from collections import OrderedDict, defaultdict
-import json
 import os
 import sys
-# 3rd party
+import biom
+import argparse
 try:
-    import matplotlib
+    import matplotlib.pyplot as plt
 except ImportError as ie:
-    sys.exit('Import Error. Please install missing module: {}'.format(ie))
-import matplotlib
-matplotlib.use("Agg")  # for use on headless server
-import matplotlib.pylab as p
-# local
-from phylotoast import util, graph_util
+    sys.exit("Import Error. Please install missing module: {}".format(ie))
+from phylotoast import util, graph_util as gu, biom_calc as bc, otu_calc as oc
 
 
-def otu_biom_entry_num(ID, biom, entry_type='rows'):
-    for i, entry in enumerate(biom[entry_type]):
-        if entry['id'] == ID:
-            return i
-
-
-def rel_abundance(otuID, sampleID, biom, scaling_factor=10000):
-    otuRow = otu_biom_entry_num(otuID, biom)
-    sCol = otu_biom_entry_num(sampleID, biom, 'columns')
-    otuAbundance = 0
-    sampleAbundance = 0
-
-    for row, col, amt in biom['data']:
-        if col == sCol:
-            sampleAbundance += amt
-        if col == sCol and row == otuRow:
-            otuAbundance = amt
-
-    if sampleAbundance == 0:
-        return 0
-
-    return otuAbundance / sampleAbundance * scaling_factor
-
+def get_relative_abundance(biomfile):
+    """
+    Return relative abundance from a OTU table. OTUIDs are converted to their
+    genus-species identifier.
+    """
+    biomf = biom.load_table(biomfile)
+    norm_biomf = biomf.norm(inplace=False)
+    rel_abd = {}
+    for sid in norm_biomf.ids():
+        rel_abd[sid] = {}
+        for otuid in norm_biomf.ids("observation"):
+            otuname = oc.otu_name(norm_biomf.metadata(otuid, axis="observation")["taxonomy"])
+            abd = norm_biomf.get_value_by_ids(otuid, sid)
+            rel_abd[sid][otuname] = abd
+    return rel_abd
 
 def calculate_xy_range(data):
-    xr = [float('inf'), float('-inf')]
-    yr = [float('inf'), float('-inf')]
+    xr = [float("inf"), float("-inf")]
+    yr = [float("inf"), float("-inf")]
 
     for cat in data:
-        pc1, pc2 = data[cat]['pc1'], data[cat]['pc2']
+        pc1, pc2 = data[cat]["pc1"], data[cat]["pc2"]
         if pc1:
             xr[0] = min(min(pc1), xr[0])
             xr[1] = max(max(pc1), xr[1])
@@ -64,101 +49,77 @@ def calculate_xy_range(data):
     return xr, yr
 
 
-def link_samples_to_categories(imap, category_idx):
+def plot_PCoA(cat_data, otu_name, unifrac, names, colors, xr, yr, outDir,
+              save_as, plot_style):
     """
-    Creates a dictionary of category types with all the associated Sample IDs
-
-    :type imap: dict
-    :param imap: The mapping data that lists category information
-    :type category_idx: int
-    :param category_idx: The index in the mapping data of the user-specified
-                          category
-    :rtype: OrderedDict
-    :return: A dictionary of category types mapped to a list of corresponding
-              sample IDs
+    Plot PCoA principal coordinates scaled by the relative abundances of
+    otu_name.
     """
-    cat_types = defaultdict(list)
-    for row in imap.values():
-        cat_types[row[category_idx]].append(row[0])
-    return OrderedDict(sorted(cat_types.items(), key=lambda t: t[0]))
-
-
-def plot_PCoA(cat_data, otu_name, unifrac, names, colors, xr, yr, outDir):
-    p.matplotlib.rc('axes', edgecolor='black')
-    p.matplotlib.rc('axes', facecolor='grey')
-    fig = p.plt.figure()
+    fig = plt.figure()
     ax = fig.add_subplot(111)
-#    fig.set_figheight(8)
-#    fig.set_figwidth(10)
     legend = []
 
     for i, cat in enumerate(cat_data):
-        p.scatter(cat_data[cat]['pc1'], cat_data[cat]['pc2'],
-                  cat_data[cat]['size'], color=colors[i], alpha=0.85, marker='o',
-                  edgecolor='black')
-#        p.scatter(cat_data[cat]['zpc1'], cat_data[cat]['zpc2'], s=20,
-#                  edgecolor=colors[i], facecolor='none', marker='s')
-        legend.append(p.Rectangle((0, 0), 1, 1, fc=colors[i]))
+        plt.scatter(cat_data[cat]["pc1"], cat_data[cat]["pc2"],
+                    cat_data[cat]["size"], color=colors[cat],
+                    alpha=0.85, marker="o", edgecolor="black")
+        legend.append(plt.Rectangle((0, 0), 1, 1, fc=colors[cat]))
 
-    ax.legend(legend, names, loc='best')
-    p.title(otu_name, style='italic')
-    p.ylabel('PC2 ({:.2f}%)'.format(float(unifrac['varexp'][2])))
-    p.xlabel('PC1 ({:.2f}%)'.format(float(unifrac['varexp'][1])))
-    p.xlim(round(xr[0]*1.5, 1), round(xr[1]*1.5, 1))
-    p.ylim(round(yr[0]*1.5, 1), round(yr[1]*1.5, 1))
-    graph_util.rstyle(ax)
-    fig.savefig(os.path.join(outDir, '_'.join(otu_name.split())) + '.png',
-                facecolor='0.75', edgecolor='none')
-    p.plt.close(fig)
+    ax.legend(legend, names, loc="best")
+    plt.title(" ".join(otu_name.split("_")), style="italic")
+    plt.ylabel("PC2 - Percent variation explained {:.2f}%".format(float(unifrac["varexp"][2])))
+    plt.xlabel("PC1 - Percent variation explained {:.2f}%".format(float(unifrac["varexp"][1])))
+    plt.xlim(round(xr[0]*1.5, 1), round(xr[1]*1.5, 1))
+    plt.ylim(round(yr[0]*1.5, 1), round(yr[1]*1.5, 1))
+    if plot_style:
+        gu.ggplot2_style(ax)
+        fc = "0.8"
+    else:
+        fc = "none"
+    fig.savefig(os.path.join(outDir, "_".join(otu_name.split())) + "." + save_as,
+                facecolor=fc, edgecolor="none", format=save_as,
+                bbox_inches="tight", pad_inches=0.2)
+    plt.close(fig)
 
 
 def handle_program_options():
-    parser = argparse.ArgumentParser(description="Create a series of Principle\
+    parser = argparse.ArgumentParser(description="Create a series of Principal\
                                      Coordinate plots for each OTU in an \
                                      input list where the plot points are \
                                      varied in size by the relative abundance \
-                                     of the OTU (relative to either Sample or\
+                                     of the OTU relative to either Sample or\
                                      the total contribution of the OTU to the \
                                      data set.")
-    parser.add_argument('-i', '--otu_table', required=True,
+    parser.add_argument("-i", "--otu_table", required=True,
                         help="The biom-format file with OTU-Sample abundance \
                               data.")
-    parser.add_argument('-u', '--unifrac', required=True,
-                        help='Principle coordinates analysis file. \
-                              Eg. unweighted_unifrac_pc.txt')
-    parser.add_argument('-d', '--names_colors_ids_fn', required=True,
-                        help='The name of an input data file containing three\
-                              items: Line 1: a tab-separated list of display\
-                              names for the different types in the specified \
-                              mapping category (--mapping_category), Line 2:\
-                              a matching tab-separated list of hexadecimal\
-                              colors for each of the category types, \
-                              Lines 3-end: a tab-separated pair specifying \
-                              OTU ID and OTU Name. Each entry will get a \
-                              separate PCoA plot under a file with the name \
-                              of the OTU.')
-    parser.add_argument('-m', '--mapping', required=True,
+    parser.add_argument("-m", "--mapping", required=True,
                         help="The mapping file specifying group information \
                               for each sample.")
-    parser.add_argument('-c', '--map_category', required=True,
-                        help="Any mapping category, such as treatment type, \
-                              that will be used to group the data in the \
-                              output plots. For example, one category \
-                              with three types will result in three different\
-                              point sets in the final output.")
-    parser.add_argument('-o', '--output_dir', default='.',
+    parser.add_argument("-u", "--unifrac", required=True,
+                        help="Principal coordinates analysis file. \
+                              Eg. unweighted_unifrac_pc.txt")
+    parser.add_argument("-b", "--group_by", required=True,
+                        help="Column name in mapping file specifying group information.")
+    parser.add_argument("-c", "--colors", default=None,
+                        help="A column name in the mapping file containing\
+                              hexadecimal (#FF0000) color values that will\
+                              be used to color the groups. Each sample ID must\
+                              have a color entry.")
+    parser.add_argument("-d", "--names_colors_ids_fn", required=True,
+                        help="File containing one OTU name per line for plotting.")
+    parser.add_argument("-o", "--output_dir", default=".",
                         help="The directory to output the PCoA plots to.")
-
-    parser.add_argument('--scaling_factor', default=10000, type=float,
+    parser.add_argument("-s", "--save_as", default="svg",
+                        help="The type of image file for PCoA plots. By default\
+                              , files will be saved in SVG format.")
+    parser.add_argument("--scale_by", default=10000, type=float,
                         help="Species relative abundance is multiplied by this \
                               factor in order to make appropriate visible \
                               bubbles in the output plots. Default is 10000.")
-#    parser.add_argument('-n', '--normalization', default='sample',
-#                        choices=['sample','otu'],
-#                        help="Specifies whether OTU abundance is \
-#                        normalized column-wise (per-sample) or row-wise \
-#                        (per-OTU).")
-    parser.add_argument('-v', '--verbose', action='store_true',
+    parser.add_argument("--ggplot2_style", action="store_true",
+                        help="Apply ggplot2 styling to the figure.")
+    parser.add_argument("-v", "--verbose", action="store_true",
                         help="Displays species name as each is being plotted \
                               and stored to disk.")
 
@@ -172,83 +133,78 @@ def main():
         with open(args.otu_table):
             pass
     except IOError as ioe:
-        sys.exit('\nError with OTU/Sample abundance BIOM format file:{}\n'.format(ioe))
+        sys.exit("\nError with BIOM format file:{}\n".format(ioe))
 
     try:
         with open(args.unifrac):
             pass
     except IOError as ioe:
-        sys.exit('\nError with principle coordinates analysis file:{}\n'.format(ioe))
-
-    try:
-        with open(args.names_colors_ids_fn):
-            pass
-    except IOError as ioe:
-        sys.exit('\nError with input data file:{}\n'.format(ioe))
+        sys.exit("\nError with principal coordinates file:{}\n".format(ioe))
 
     try:
         with open(args.mapping):
             pass
     except IOError as ioe:
-        sys.exit('\nError with mapping file:{}\n'.format(ioe))
+        sys.exit("\nError with mapping file:{}\n".format(ioe))
 
     if not os.path.exists(args.output_dir):
         try:
             os.mkdir(args.output_dir)
         except OSError as oe:
             if os.errno == 2:
-                msg = ('One or more directories in the path provided for ' +
-                       '--output-dir ({}) do not exist. If you are specifying ' +
-                       'a new directory for output, please ensure all other ' +
-                       'directories in the path currently exist.')
+                msg = ("One or more directories in the path provided for " +
+                       "--output-dir ({}) do not exist. If you are specifying " +
+                       "a new directory for output, please ensure all other " +
+                       "directories in the path currently exist.")
                 sys.exit(msg.format(args.output_dir))
             else:
-                msg = ('An error occurred trying to create the output ' +
-                       'directory ({}) with message: {}')
+                msg = ("An error occurred trying to create the output " +
+                       "directory ({}) with message: {}")
                 sys.exit(msg.format(args.output_dir, oe.strerror))
 
-    with open(args.otu_table) as bF:
-        biom = json.loads(bF.readline())
-
+    # Read unifrac principal coordinates file
     unifrac = util.parse_unifrac(args.unifrac)
 
-    otus = {}
-    with open(args.names_colors_ids_fn, 'rU') as nciF:
-        category_names = nciF.readline().strip().split('\t')
-        category_colors = nciF.readline().strip().split('\t')
+    # Read otu data file
+    otus = set()
+    with open(args.names_colors_ids_fn, "rU") as nciF:
         for line in nciF.readlines():
-            line = line.split()
-            otus[line[0]] = ' '.join(line[1:])
-    header, imap = util.parse_map_file(args.mapping)
+            line = line.strip()
+            otus.add(line)
 
+    # Gather categories from mapping file
+    header, imap = util.parse_map_file(args.mapping)
     try:
-        category_idx = header.index(args.map_category)
+        category_idx = header.index(args.group_by)
     except ValueError:
-        msg = "Error: Specified mapping category '{}' not found"
-        sys.exit(msg.format(args.map_category))
-    category_ids = link_samples_to_categories(imap, category_idx)
+        msg = "Error: Specified mapping category '{}' not found."
+        sys.exit(msg.format(args.group_by))
+    category_ids = util.gather_categories(imap, header, [args.group_by])
+    color_map = util.color_mapping(imap, header, args.group_by, args.colors)
+    rel_abd = get_relative_abundance(args.otu_table)
 
     # plot samples based on relative abundance of some OTU ID
-    for otuID in otus:
-        cat_data = {cat: {'pc1': [], 'pc2': [], 'size': [], 'zpc1': [], 'zpc2': []}
+    for otuname in otus:
+        cat_data = {cat: {"pc1": [], "pc2": [], "size": []}
                     for cat in category_ids}
 
-        for sid in unifrac['pcd']:
+        for sid in unifrac["pcd"]:
             category = cat_data[imap[sid][category_idx]]
-            size = rel_abundance(otuID, sid, biom, args.scaling_factor)
-            # if size > 0:
-            category['pc1'].append(float(unifrac['pcd'][sid][0]))
-            category['pc2'].append(float(unifrac['pcd'][sid][1]))
-            category['size'].append(size)
-#            else:
-#                category['zpc1'].append(float(unifrac['pcd'][sid][1]))
-#                category['zpc2'].append(float(unifrac['pcd'][sid][2]))
+            try:
+                size = rel_abd[sid][otuname] * args.scale_by
+            except KeyError as ke:
+                print "{} not found in {} sample.".format(ke, sid)
+                continue
+            category["pc1"].append(float(unifrac["pcd"][sid][0]))
+            category["pc2"].append(float(unifrac["pcd"][sid][1]))
+            category["size"].append(size)
 
         if args.verbose:
-            print 'Plotting chart for {}'.format(otus[otuID])
+            print "Saving chart for {}".format(" ".join(otuname.split("_")))
         xr, yr = calculate_xy_range(cat_data)
-        plot_PCoA(cat_data, otus[otuID], unifrac, category_names,
-                  category_colors, xr, yr, args.output_dir)
+        plot_PCoA(cat_data, otuname, unifrac, color_map.keys(),
+                  color_map, xr, yr, args.output_dir,
+                  args.save_as, args.ggplot2_style)
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    sys.exit(main())
