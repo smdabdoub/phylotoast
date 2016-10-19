92c5bc89c8e48487e8bd384d91c5922c4be51d63
Akshay Paropkari <paropkari.1@gmail.com>
2016-10-19 15:34:30 -0400

    Updated API and its test script
    Updated biom_calc to be compatible with Python 2 and 3.
    Similar changes for test_biom_calc script.

--- phylotoast/biom_calc.py
+++ phylotoast/biom_calc.py
@@ -1,11 +1,8 @@
 """
-Created on Feb 19, 2013
-
-:author: Shareef Dabdoub
-
-This module provides methods for calculating various metrics with regards to
-each OTU in an input OTU abundance table. This is currently used by iTol.py
-to offload the different methods.
+:Date: Created on Feb 19, 2013
+:Author: Shareef Dabdoub
+:Abstract: This module provides methods for calculating various metrics with regards to
+           each OTU in an input OTU abundance table.
 """
 import math
 from collections import defaultdict
@@ -22,9 +19,9 @@ def relative_abundance(biomf, sampleIDs=None):
     :param sampleIDs: A list of sample id's from BIOM format OTU table.
 
     :rtype: dict
-    :return: Returns a keyed on SampleIDs, and the values are dictionaries
-             keyed on OTUID's and their values represent the relative
-             abundance of that OTUID in that SampleID.
+    :return: Returns a keyed on SampleIDs, and the values are dictionaries keyed on
+             OTUID's and their values represent the relative abundance of that OTUID in
+             that SampleID.
     """
     if sampleIDs is None:
         sampleIDs = biomf.ids()
@@ -34,9 +31,9 @@ def relative_abundance(biomf, sampleIDs=None):
                 assert sid in biomf.ids()
         except AssertionError:
             raise ValueError(
-                "\nError while calculating relative abundances: The sampleIDs "
-                "provided do not match the sampleIDs in biom file. Please "
-                "double check the sampleIDs provided.\n")
+                "\nError while calculating relative abundances: The sampleIDs provided do"
+                " not match the sampleIDs in biom file. Please double check the sampleIDs"
+                " provided.\n")
     otuIDs = biomf.ids(axis="observation")
     norm_biomf = biomf.norm(inplace=False)
 
@@ -49,32 +46,30 @@ def mean_otu_pct_abundance(ra, otuIDs):
     Calculate the mean OTU abundance percentage.
 
     :type ra: Dict
-    :param ra: 'ra' refers to a dictionary keyed on SampleIDs, and the values
-                are dictionaries keyed on OTUID's and their values represent
-                the relative abundance of that OTUID in that SampleID. 'ra' is
-                the output of relative_abundance() function.
+    :param ra: 'ra' refers to a dictionary keyed on SampleIDs, and the values are
+               dictionaries keyed on OTUID's and their values represent the relative
+               abundance of that OTUID in that SampleID. 'ra' is the output of
+               relative_abundance() function.
 
     :type otuIDs: List
-    :param otuIDs: A list of OTUID's for which the percentage abundance needs
-                   to be measured.
+    :param otuIDs: A list of OTUID's for which the percentage abundance needs to be
+                   measured.
 
     :rtype: dict
-    :return: A dictionary of OTUID and their percent relative
-                      abundance as key/value pair.
+    :return: A dictionary of OTUID and their percent relative abundance as key/value pair.
     """
     sids = ra.keys()
     otumeans = defaultdict(int)
 
     for oid in otuIDs:
         otumeans[oid] = sum([ra[sid][oid] for sid in sids
-                            if oid in ra[sid]]) / len(sids) * 100
-
+                             if oid in ra[sid]]) / len(sids) * 100
     return otumeans
 
 
 def MRA(biomf, sampleIDs=None, transform=None):
     """
-    Calculate the mean relative abundance.
+    Calculate the mean relative abundance percentage.
 
     :type biomf: A BIOM file.
     :param biomf: OTU table format.
@@ -83,8 +78,8 @@ def MRA(biomf, sampleIDs=None, transform=None):
     :param sampleIDs: A list of sample id's from BIOM format OTU table.
 
     :rtype: dict
-    :return: A dictionary keyed on OTUID's and their mean relative abundance
-             for a given number of sampleIDs.
+    :return: A dictionary keyed on OTUID's and their mean relative abundance for a given
+             number of sampleIDs.
     """
     ra = relative_abundance(biomf, sampleIDs)
     if transform is not None:
@@ -101,17 +96,16 @@ def raw_abundance(biomf, sampleIDs=None, sample_abd=True):
     :param biomf: OTU table format.
 
     :type sampleIDs: List
-    :param sampleIDs: A list of column id's from BIOM format OTU table. By
-                      default, the list has been set to None.
+    :param sampleIDs: A list of column id's from BIOM format OTU table. By default, the
+                      list has been set to None.
 
     :type sample_abd: Boolean
-    :param sample_abd: A boolean operator to provide output for OTUID's or
-                       SampleID's. By default, the output will be provided
-                       for SampleID's.
+    :param sample_abd: A boolean operator to provide output for OTUID's or SampleID's. By
+                       default, the output will be provided for SampleID's.
 
     :rtype: dict
-    :return: Returns a dictionary keyed on either OTUID's or SampleIDs
-                     and their respective abundance as values.
+    :return: Returns a dictionary keyed on either OTUID's or SampleIDs and their
+             respective abundance as values.
     """
     results = defaultdict(int)
     if sampleIDs is None:
@@ -122,9 +116,9 @@ def raw_abundance(biomf, sampleIDs=None, sample_abd=True):
                 assert sid in biomf.ids()
         except AssertionError:
             raise ValueError(
-                "\nError while calculating raw total abundances: The "
-                "sampleIDs provided do not match the sampleIDs in biom file. "
-                "Please double check the sampleIDs provided.\n")
+                "\nError while calculating raw total abundances: The sampleIDs provided "
+                "do not match the sampleIDs in biom file. Please double check the "
+                "sampleIDs provided.\n")
     otuIDs = biomf.ids(axis="observation")
 
     for sampleID in sampleIDs:
@@ -137,24 +131,21 @@ def raw_abundance(biomf, sampleIDs=None, sample_abd=True):
     return results
 
 
-def transform_raw_abundance(biomf, fn=math.log10, sampleIDs=None,
-                            sample_abd=True):
+def transform_raw_abundance(biomf, fn=math.log10, sampleIDs=None, sample_abd=True):
     """
-    Function to transform the total abundance calculation for each sample ID
-    to another format based on user given transformation function.
+    Function to transform the total abundance calculation for each sample ID to another
+    format based on user given transformation function.
 
     :type biomf: A BIOM file.
     :param biomf: OTU table format.
 
-    :param fn: Mathematical function which is used to transform smax to
-               another format. By default, the function has been given as
-               base 10 logarithm.
+    :param fn: Mathematical function which is used to transform smax to another format.
+               By default, the function has been given as base 10 logarithm.
 
     :rtype: dict
-    :return: Returns a dictionary similar to output of raw_abundance function
-             but with the abundance values modified by the mathematical
-             operation. By default, the operation performed on the abundances
-             is base 10 logarithm.
+    :return: Returns a dictionary similar to output of raw_abundance function but with
+             the abundance values modified by the mathematical operation. By default, the
+             operation performed on the abundances is base 10 logarithm.
     """
     totals = raw_abundance(biomf, sampleIDs, sample_abd)
     return {sid: fn(abd) for sid, abd in totals.items()}
